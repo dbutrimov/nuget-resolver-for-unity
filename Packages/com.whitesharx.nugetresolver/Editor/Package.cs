4faa10f0abafe4659ef77910de0d18574df6ebb7
Dmitry Butrimov <butrimov.d@whitesharx.com>
2021-12-06 18:51:17 +0300

    Fix paths

---
+++ Packages/com.whitesharx.nugetresolver/Editor/Package.cs
@@ -0,0 +1,6 @@
+namespace NuGetResolver.Editor {
+  internal static class Package {
+    public const string Id = "com.whitesharx.nugetresolver";
+    public const string BasePath = "Packages/" + Id;
+  }
+}
